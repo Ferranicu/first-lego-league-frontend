98fbbf00650b27e5db9efb45d9f4d57b9b4d676f
Cesc.UDL <fsr2@alumnes.udl.cat>
2026-04-08 19:01:21 +0200

    nextjs config fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-// next.config.ts
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "https://api.firstlegoleague.win/:path*",
-            },
-        ];
-    },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-export default nextConfig;
 export default nextConfig;
